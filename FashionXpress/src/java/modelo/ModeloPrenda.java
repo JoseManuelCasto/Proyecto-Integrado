cdb56917de521a84056e6a5307b6cd6e137bb765
JoseManuelCasto <130976031+JoseManuelCasto@users.noreply.github.com>
2024-04-21 15:52:35 +0200

    Add files via upload
    Commit 30%

---
+++ FashionXpress/src/java/modelo/ModeloPrenda.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import modelo.dao.PedidoJpaController;
+import modelo.dao.PrendaJpaController;
+import modelo.entidades.Pedido;
+import modelo.entidades.Prenda;
+import modelo.entidades.Proveedor;
+import modelo.entidades.Usuario;
+
+/**
+ *
+ * @author casto
+ */
+public class ModeloPrenda {
+
+    public static List<Prenda> obtenerPrendas() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("FashionXpressPU");
+        PrendaJpaController pjc = new PrendaJpaController(emf);
+        List<Prenda> prendas = pjc.findPrendaEntities();
+        emf.close();
+        return prendas;
+
+    }
+
+    public static void eliminarPrenda(Long id) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("FashionXpressPU");
+        PrendaJpaController pjc = new PrendaJpaController(emf);
+        try {
+            pjc.destroy(id);
+            emf.close();
+        } catch (Exception e) {
+            System.out.println("Error en ModeloPrenda.java-----" + e.getMessage());
+        }
+
+    }
+
+//Se podra editar proveedor?
+    public static void editarPrenda(String nombre, Proveedor proveedor, List<String> tallas_disp, String descripcion, double precio, String imagenPrenda, Prenda p) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("FashionXpressPU");
+        PrendaJpaController pjc = new PrendaJpaController(emf);
+
+        p.setNombre(nombre);
+        p.setProveedor(proveedor);
+        p.setTallas_disp(tallas_disp);
+        p.setDescripcion(descripcion);
+        p.setPrecio(precio);
+        p.setImagenPrenda(imagenPrenda);
+
+        try {
+            pjc.edit(p);
+            emf.close();
+        } catch (Exception e) {
+            System.out.println("Error en ModeloPrenda.java ---------" + e.getMessage());
+        }
+
+    }
+
+    public static void anadirPrenda(String nombre, Proveedor proveedor, List<String> tallas_disp, String descripcion, double precio, String imagenPrenda) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("FashionXpressPU");
+        PrendaJpaController pjc = new PrendaJpaController(emf);
+        Prenda p = new Prenda();
+        
+        p.setNombre(nombre);
+        p.setProveedor(proveedor);
+        p.setTallas_disp(tallas_disp);
+        p.setDescripcion(descripcion);
+        p.setPrecio(precio);
+        p.setImagenPrenda(imagenPrenda);
+        
+        try {
+            pjc.create(p);
+            emf.close();
+        } catch (Exception e) {
+            System.out.println("Error en ModeloPrenda.java ---------" + e.getMessage());
+        }
+
+    }
+
+    public static Prenda obtenerPrenda(Long id) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("FashionXpressPU");
+        PrendaJpaController pjc = new PrendaJpaController(emf);
+        Prenda p = pjc.findPrenda(id);
+        emf.close();
+        return p;
+
+    }
+}
---
+++ FashionXpress/src/java/modelo/entidades/Prenda.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo.entidades;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author casto
+ */
+@Entity
+public class Prenda implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    private Proveedor proveedor;
+    @Column(nullable = false)
+    private String nombre;
+    @ElementCollection
+    private List<String> tallas_disp;
+    @Column(nullable = false)
+    private String descripcion;
+    @Column(nullable = false)
+    private double precio;
+    @Column(nullable = false)
+    private String imagenPrenda;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<String> getTallas_disp() {
+        return tallas_disp;
+    }
+
+    public void setTallas_disp(List<String> tallas_disp) {
+        this.tallas_disp = tallas_disp;
+    }
+
+    public String getImagenPrenda() {
+        return imagenPrenda;
+    }
+
+    public void setImagenPrenda(String imagenPrenda) {
+        this.imagenPrenda = imagenPrenda;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Prenda)) {
+            return false;
+        }
+        Prenda other = (Prenda) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.entidades.Prenda[ id=" + id + " ]";
+    }
+
+}
